d12995e3796d919bdac9fe9c9a46d4d8cdf5ba58
Adam David Scott <adam@adamscottphd.com>
2016-01-07 11:47:08 -0600

    massive restructuring. new setup is a annotated variant file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CharGer
 Characterization of Germline variants
+## Dependencies
+### Latest WebAPIs
+CharGer is still under construction, so a one step install is not yet 
+available. CharGer requires modules developed for several web API's developed 
+by AdamDS. These can be installed by getting the latest commit of 
+AdamDS/WebAPIs, found here
+#### git clone https://github.com/AdamDS/WebAPIs.git
+It may be necessary to periodically update the version of these modules, so if 
+you already have the WebAPIs repository, in your local repository of WebAPIs, 
+use the command:
+#### git pull origin master
+Once you have the latest WebAPIs, in the head directory of the repository, 
+type:
+#### python setup.py install
+## Run
+CharGer will (at some point) be able to run with only variant input OR with 
+variant input and gene lists.
+### Without gene lists
+#### python charger.py -i <variant file> -o <output file>
---
+++ charger.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+# CharGer - Characterization of Germline variants
+# author: Adam D Scott (ascott@genome.wustl.edu)
+# version: v0.0 - 2015*12
+
+import sys
+import getopt
+from entrezAPI import entrezAPI
+import variant
+
+def parseArgs( argv ):
+	helpText = "python main.py" + " "
+	helpText += "-i \"input (.maf)\" "
+	helpText += "(-o \"output\")\n"
+	inputFile = ""
+	output = ""
+	database = ""
+	query = ""
+	try:
+		opts, args = getopt.getopt( argv , "h:i:o:" , ["input=" , "output="] )
+	except getopt.GetoptError:
+		print "ADSERROR: Command not recognized"
+		print( helpText ) 
+		sys.exit(2)
+	if not opts:
+		print "ADSERROR: Expected flagged input"
+		print( helpText ) 
+		sys.exit(2)
+	for opt, arg in opts:
+		#print opt + " " + arg
+		if opt in ( "-h" , "--help" ):
+			print( helpText )
+			sys.exit()
+		elif opt in ( "-i" , "--inputFile" ):
+			inputFile = arg
+		elif opt in ( "-o" , "--output" ):
+			output = arg
+	return { "input" : inputFile , "output" : output }
+	
+def checkConnection():
+	print "\tChecking Connection!"
+	entrezInstance = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=asthma[mesh]+AND+leukotrienes[mesh]+AND+2009[pdat]"
+	summaryTest = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=20113659,20074456"
+	res = requests.get( entrezInstance )
+	if res:
+		print "have response"
+	else:
+		print res.status_code
+
+def splitByVariantType( inputFile ):
+	print "\tSplitting .maf by variant type!"
+	variants = {}
+	if inputFile:
+		inFile = open( inputFile , 'r' )
+		next(inFile)
+		gv = {}
+		for line in inFile:
+			fields = line.split( "\t" )
+			var = variant.variant()
+			var.mafLine2Variant( line )
+			#var.printVariant("\t")
+			sample = fields[15]
+			variantClass = var.variantClass
+			genVar = var.genomicVar()
+			vc = {}
+			s = {}
+			gv = {}
+			if variantClass in variants:
+				#print variantClass
+				vc = variants[variantClass] #dict of variants by variantClass=>samples dict
+			if sample in vc:
+				#print sample
+				s = vc[sample] #dict of samples by sample=>genVars dict
+			if genVar in s:
+				#print genVar
+				gv = s[genVar]
+			tempgv = { "line" : line , "variant" : var }
+			gv.update( tempgv )
+			temps = { genVar : gv }
+			s.update( temps )
+			tempvc = { sample : s }
+			vc.update( tempvc )
+			#print genVar + "=>" + s[genVar]
+			tempVariants = { variantClass : tempvc }
+			variants.update( tempVariants )
+	return variants
+	#nonsense, frameshift, canonical 1 or 2 splice sites, initiation codon, single exon or multiexon deletion
+
+def PVS1( variants ):
+	return None
+	
+def main( argv ):
+	values = parseArgs( argv )
+	inputFile = values["input"]
+	outputFormat = values["output"]
+
+	ent = entrezAPI()	
+
+	variants = splitByVariantType( inputFile )
+	for variantClass in variants:
+		#print "\tVariant Class: " + variantClass
+		for sample in variants[variantClass]:
+			#print "\t\tSample: " + sample
+			#variants[variantClass][sample][
+			for genVar in variants[variantClass][sample]:
+				#print "\t\t\tGenomic Variant: " + genVar
+				#print "\t\t\t\tVariant: " , 
+				#variants[variantClass][sample][genVar]["variant"].printVariant("  ")
+				thisGroup = sample+genVar
+				var = variants[variantClass][sample][genVar]["variant"]
+				ent.addQuery( var.gene , field="gene" , group=thisGroup )
+				ent.addQuery( var.chromosome , field="chr" , group=thisGroup )
+				ent.addQuery( var.start + ":" + var.stop , field="chrpos37" , group=thisGroup )
+				ent.addQuery( "human" , field="orgn" , group=thisGroup )
+				#ent.addQuery( var.variantClass , "vartype" )
+				#ent.addQuery( var.referencePeptide + var.positionPeptide + var.mutantPeptide , "Variant name" )
+	ent.database = entrezAPI.clinvar
+	ent.doBatch( 5 )
+
+if __name__ == "__main__":
+	main( sys.argv[1:] )
