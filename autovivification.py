d53299de0b8ad13a43b6338c301cfa129d233944
Adam David Scott <adam@adamscottphd.com>
2016-01-13 10:35:55 -0600

    fixed autovivification. changed module call keys to uniqueVar

---
+++ autovivification.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# autovivification - extends dict
+# author: Kuan-lin Huang (khuang@genome.wustl.edu) & Adam D Scott (ascott@genome.wustl.edu)
+# version: v0.0 - 2016*01*12
+
+class autovivification(dict):
+	'''Implementation of perl's autovivification feature.'''
+	def __init__( self , *args , **kwargs ):
+		super( autovivification , self ).__init__( *args , **kwargs )
+		self.itemlist = super( autovivification , self ).keys()
+	def __getitem__(self, item):
+		try:
+			return dict.__getitem__(self, item)
+		except KeyError:
+			value = self[item] = type(self)()
+			return value
