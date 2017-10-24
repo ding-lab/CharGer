daf963736a624d3560903800340ddfc81aff1f71
Adam David Scott <adam@adamscottphd.com>
2017-10-24 15:22:39 -0500

    prelim SV handling to readVCF

--- charger/charger.py
+++ charger/charger.py
@@ -205,66 +205,76 @@ class charger(object):
 		failedMT = 0
 		[ vepDone , exacDone , clinvarDone ] = self.readMetaData( metadata , inFile.infos , vepInfo )
 		for record in inFile:
-			chrom = record.CHROM
-			reference = record.REF
-			alternates = record.ALT
-			if anyFilter and record.FILTER != "PASS":
-				failedFilter += 1
-				continue
-			start = record.start + 1 #1-base beginning of ref
-			stop = record.end #0-base ending of ref
-			info = record.INFO
-			alti = -1
-			for alternate in alternates:
-				alti += 1
-				alt = str( alternate )
-				if alt == "None":
-					alt = None
-				if record.is_indel and not record.is_deletion: #insertion
-					reference = "-"
-					alt = alt[1:len(alt)]
-					stop = stop + 1
-				elif record.is_deletion:
-					reference = reference[1:len(reference)] #assumes only one base overlap
-					alt = "-"
-					start = start + 1
-					stop = stop
+			try:
+				chrom = record.CHROM
+				reference = record.REF
+				alternates = record.ALT
+				if anyFilter and record.FILTER != "PASS":
+					failedFilter += 1
+					continue
+				start = record.start + 1 #1-base beginning of ref
+				stop = record.end #0-base ending of ref
+				info = record.INFO
+				alti = -1
+				for alternate in alternates:
+					alti += 1
+					alt = str( alternate )
+					if alt == "None":
+						alt = None
+					if record.is_sv:
+						print( "CharGer::readVCF - SV detected" )
+						print( record )
+						if not record.is_sv_precise:
+							print( "CharGer::readVCF - SV is not precise" )
+							print( record )
+					elif record.is_indel and not record.is_deletion: #insertion
+						reference = "-"
+						alt = alt[1:len(alt)]
+						stop = stop + 1
+					elif record.is_deletion:
+						reference = reference[1:len(reference)] #assumes only one base overlap
+						alt = "-"
+						start = start + 1
+						stop = stop
 
-				parentVar = mafvariant( \
-					chromosome = chrom , \
-					start = start , \
-					stop = stop , \
-					dbsnp = record.ID , \
-					reference = reference , \
-					alternate = alt , \
-				)
+					parentVar = mafvariant( \
+						chromosome = chrom , \
+						start = start , \
+						stop = stop , \
+						dbsnp = record.ID , \
+						reference = reference , \
+						alternate = alt , \
+					)
 
-				var = chargervariant( \
-					parentVariant=parentVar
-				)
+					var = chargervariant( \
+						parentVariant=parentVar
+					)
 
-				var.disease = charger.allDiseases
+					var.disease = charger.allDiseases
 
-				hasAF = False
-				hasAF = self.getAF( info , var , alti )
+					hasAF = False
+					hasAF = self.getAF( info , var , alti )
 #				#print( str( var.alleleFrequency ) )
 #				hasClinVar = False
 #				ClinVarElsewhere = info.get( 'clinvar_measureset_id' , 0 )
 #				var.clinvarVariant.clinical["description"] = info.get( 'clinvar_measureset_id' , "noClinVar" )
 #				var.clinvarVariant.uid = info.get( 'clinvar_measureset_id' , "noClinVar" )
 #				if ClinVarElsewhere != 0:
-				self.getVEPConsequences( info , var , preVEP , hasAF )
-				if self.skipIfHighAF( var ):
-					#self.filtered.append( var.vcf() )
-					failedAF += 1
-					continue
-				if self.skipIfNotInMutationTypes( var ):
-					#self.filtered.append( var.vcf() )
-					failedMT += 1
-					continue
-					
-				variantDict = self.appendToList( appendTo , var , \
-												 variantDict = variantDict )
+					self.getVEPConsequences( info , var , preVEP , hasAF )
+					if self.skipIfHighAF( var ):
+						#self.filtered.append( var.vcf() )
+						failedAF += 1
+						continue
+					if self.skipIfNotInMutationTypes( var ):
+						#self.filtered.append( var.vcf() )
+						failedMT += 1
+						continue
+						
+					variantDict = self.appendToList( appendTo , var , \
+													 variantDict = variantDict )
+			except:
+				print( "CharGer::readVCF - something about this variant record:\n\t" )
+				print( record )
 		totalVars = len( self.userVariants ) + failedFilter + failedAF + failedMT
 		print(  "Skipping: " + str( failedFilter ) + " for filters and " + \
 				str( failedAF ) + " for AF and " + \
