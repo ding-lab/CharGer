619a6705c0a4b2bf8edc2da08533769f59e6e25c
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2020-02-24 10:01:48 -0600

    Add all Ensembl v99 VEP variant effects

--- src/charger/variant.py
+++ src/charger/variant.py
@@ -428,3 +428,47 @@ class ClinicalSignificance(Enum):
                     clin_sig = cls.PATHOGENIC
                     break
         return clin_sig
+
+
+ALL_VEP_VARIANT_EFFECTS = [
+    "transcript_ablation",
+    "splice_acceptor_variant",
+    "splice_donor_variant",
+    "stop_gained",
+    "frameshift_variant",
+    "stop_lost",
+    "start_lost",
+    "transcript_amplification",
+    "inframe_insertion",
+    "inframe_deletion",
+    "missense_variant",
+    "protein_altering_variant",
+    "splice_region_variant",
+    "incomplete_terminal_codon_variant",
+    "start_retained_variant",
+    "stop_retained_variant",
+    "synonymous_variant",
+    "coding_sequence_variant",
+    "mature_miRNA_variant",
+    "5_prime_UTR_variant",
+    "3_prime_UTR_variant",
+    "non_coding_transcript_exon_variant",
+    "intron_variant",
+    "NMD_transcript_variant",
+    "non_coding_transcript_variant",
+    "upstream_gene_variant",
+    "downstream_gene_variant",
+    "TFBS_ablation",
+    "TFBS_amplification",
+    "TF_binding_site_variant",
+    "regulatory_region_ablation",
+    "regulatory_region_amplification",
+    "feature_elongation",
+    "regulatory_region_variant",
+    "feature_truncation",
+    "intergenic_variant",
+]
+"""All the possible VEP variant effects fetched from `Ensembl v99`_ (January 2020).
+
+.. _Ensembl v99: https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
+"""
